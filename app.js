e0e493c61365fc2fddc10d93218837e78dbe8017
Ruiz <bruiz@una.edu>
2024-08-29 13:13:00 -0500

    added doc folder

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ console.log('im on the server');
 const express = require('express')
 const app = express()
 
+app.use(express.static('./public/potential-robot-shows-app-main'))
+
 app.get('/', function (req, res) {
-  res.send('Hello Node from express on local dev box')
+  res.sendFile('index.html')
 })
+
 
 app.listen(5000)
