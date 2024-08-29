3ef38b9da2eae3cc401d87a24a1a42f7f14f747a
Ruiz <bruiz@una.edu>
2024-08-29 13:49:33 -0500

    uploaded new stuff

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ console.log('im on the server');
 
 const express = require('express')
 const app = express()
-
+app.set('view engine', 'ejs')
 app.use(express.static('./public/potential-robot-shows-app-main'))
 
 app.get('/', function (req, res) {
   res.sendFile('index.html')
 })
+
+app.get('/ejs', (res, req)=>{
+  ``
+  res.render('index',{
+    myServerVariable : "something from server"
+  });
+
+//can you get content from client...to console?
+
+})
+
 
 
 app.listen(5000)
